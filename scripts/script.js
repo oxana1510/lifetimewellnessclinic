76845ddc251982e0de02d1de98b4ea184502e2cc
oxana1510 <oksi666@xaker.ru>
2019-06-25 11:05:51 +0300

    .

---
+++ scripts/script.js
@@ -0,0 +1,30 @@
+(function() {
+ 
+  "use strict";
+ 
+  var toggles = document.querySelectorAll(".mobail-menu");
+ 
+  for (var i = toggles.length - 1; i >= 0; i--) {
+    var toggle = toggles[i];
+    toggleHandler(toggle);
+  };
+ 
+  function toggleHandler(toggle) {
+    toggle.addEventListener( "click", function(e) {
+      e.preventDefault();
+      (this.classList.contains("is-active") === true) ? this.classList.remove("is-active") : this.classList.add("is-active");
+      $(".menu").toggleClass("responsive");
+    });
+  }
+ 
+})();
+
+
+$('.slider').slick({
+  dots: false,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  adaptiveHeight: true,
+});
